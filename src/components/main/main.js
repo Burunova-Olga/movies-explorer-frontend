4cb4d8138e4b816e01495cfa2e87ac15e8cad959
Burunova Olga <burun-olya@yandex.ru>
2024-01-22 23:00:51 +0300

    nav

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -3,12 +3,14 @@
 import React from 'react';
 
 import Promo from '../promo/promo';
+import NavTab from '../nav-tab/nav-tab';
 
 function Main()
 {
   return (
     <main>
       <Promo />
+      <NavTab />
     </main>
   );
 }
--- src/components/nav-tab/nav-tab.js
+++ src/components/nav-tab/nav-tab.js
@@ -1,1 +1,18 @@
 // компонент с навигацией по странице «О проекте»
+
+import React from 'react';
+
+function NavTab()
+{
+  return (
+    <nav className="nav-tab">      
+      <ul class="nav-tab__list">
+        <li><a href="https://yandex.ru/maps" class="nav-tab__link link">О проекте</a></li>
+        <li><a href="https://yandex.ru/pogoda" class="nav-tab__link link">Технологии</a></li>
+        <li><a href="https://rasp.yandex.ru" class="nav-tab__link link">Студент</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavTab;
