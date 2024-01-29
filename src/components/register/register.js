020d04fa86135e8b956110df8b3e6ba6d0e600fb
Burunova Olga <burun-olya@yandex.ru>
2024-01-29 23:23:37 +0300

    auth-form

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,22 +1,19 @@
 // компонент страницы регистрации
-import { NavLink, Link } from 'react-router-dom';
+
 import AuthForm from '../auth-form/auth-form';
 
-function Register({onSubmit})
+function Register({ onSubmit })
 {
   return (
-    <div className='register'>
-      <AuthForm       
-        title="Добро пожаловать!" 
-        submmitText="Зарегистрироваться" 
-        onSubmit={onSubmit}  
-      >
-        <div className="auth-form__question">
-          <p>Уже зарегистрированы?</p>
-          <Link to="/signin" className="auth-form__link link">Войти</Link>
-        </div>
-      </AuthForm>
-    </div>
+    <AuthForm
+      isRegister={true}
+      title="Добро пожаловать!"
+      submmitText="Зарегистрироваться"
+      questionText="Уже зарегистрированы?"
+      linkText="Войти"
+      link="/signin"
+      onSubmit={onSubmit}
+    />
   );
 }
 
